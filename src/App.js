02bba7a94cf80096b1511f4f3749152771622302
Richa Sharma <Richasharma@BHAVNACORP.NET>
2019-05-03 18:53:14 +0530

    first component added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Button from './components/Button/component/Button';
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Button />
         </a>
       </header>
     </div>
---
+++ src/components/Button/component/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Component } from 'component';
+
+const Button = () => (
+    "New button"
+);
+
+export default Button;
